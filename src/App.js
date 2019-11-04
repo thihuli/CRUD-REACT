4d66d68a160bf87c42a1398cce697da32d7e9377
Thiago Fernandes <39868114+thihuli@users.noreply.github.com>
2019-11-04 00:00:01 -0300

    adicionando rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+/*class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>React CRUD</h2>
+      </div>
+    )
+  }
+}*/
 function App() {
   return (
     <div className="App">
