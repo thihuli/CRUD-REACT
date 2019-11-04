52032e1fb226bbcff1e19455fbd7a15f60c8eb7d
Thiago Fernandes <39868114+thihuli@users.noreply.github.com>
2019-11-03 23:30:48 -0300

    subindo projeto

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <h2> React CRUD </h2>
+      </div>
+        
+    </div>
+  );
+}
+
+export default App;
